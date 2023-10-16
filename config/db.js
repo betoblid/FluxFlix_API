bd23a94659259a2db38f7cc65503a59955815d5d
betoblid <soyzabeto@gmail.com>
2023-10-15 21:48:07 -0300

    versão 0.1.101 API_FluxFlix

---
+++ config/db.js
@@ -0,0 +1,18 @@
+require("dotenv").config();
+const postgres = require("postgres");
+
+let { PGHOST, PGDATABASE, PGUSER, PGPASSWORD, ENDPOINT_ID } = process.env;
+
+const sql = postgres({
+  host: PGHOST,
+  database: PGDATABASE,
+  username: PGUSER,
+  password: PGPASSWORD,
+  port: 5432,
+  ssl: 'require',
+  connection: {
+    options: `project=${ENDPOINT_ID}`,
+  },
+});
+
+module.exports = sql;
